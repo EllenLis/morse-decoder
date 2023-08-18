56400fd79dee2f9739dcfc20d37196d78669de0d
EllenLis <elena139@bk.ru>
2023-08-18 11:30:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let str = '';
+    let count = 0;
+
+    for (let symb of expr) {
+        str += symb;
+        count++;
+        if (count === 10) {
+            arr.push(str)
+            str = '';
+            count = 0;
+        }
+    }
+
+    return arr.map((item) => {
+        let letter = '';
+        if (item === '**********') {
+            letter = ' ';
+            return letter;
+        }
+        for (let i = 0; i <= item.length; i = i + 2) {
+            if (item[i] === '1' && item[i + 1] === '1') letter += '-';
+            if (item[i] === '1' && item[i + 1] === '0') letter += '.';
+        }
+        return letter;
+    }).map((letter) => {
+        if (letter === ' ') return ' ';
+        if (letter in MORSE_TABLE) {
+            return MORSE_TABLE[letter]
+        }
+    }).join('');
+
 }
 
 module.exports = {
